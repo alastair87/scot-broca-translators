8e47321d344016a6766053e1c90215e5e9a4f833
Alastair Cooper <alastair.cooper@outlook.com>
2019-10-09 16:11:48 +0100

    changed db name to broca_demo

--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@ const config = {
   development: {
     user: "app_user",
     host: "localhost",
-    database: "final_project",
+    database: "broca_demo",
     password: "password",
     port: 5432
   },
